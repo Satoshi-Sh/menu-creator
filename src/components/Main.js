30debbe14159078dd69e292821f6e4d423b78245
satoshi-sh <satoss1108@gmail.com>
2023-04-28 18:55:05 -0500

    Removed unnecessary props

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Menu from "./Menu";
-function Main(props) {
-  const { menu, setMenu, image } = props;
-
+function Main() {
   return (
     <>
       <Header />
